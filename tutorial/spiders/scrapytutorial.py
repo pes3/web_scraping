5c991817fc3e6b95def821f87dc4ac5176bc65b6
pes3 <patrick.soulages@gmail.com>
2018-08-19 13:02:32 -0700

    scrapy tutorial

--- tutorial/spiders/scrapytutorial.py
+++ tutorial/spiders/scrapytutorial.py
@@ -1,21 +1,20 @@
 import scrapy
 
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+
     def start_requests(self):
         urls = [
-        'http://quotes.toscrape.come/page/1/',
-        '/http://quotes.toscrape.com/page/2/'
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
         ]
-
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[:2] # checking he url , splitting it by character/extracting number after slash
-        # use page variable to define filename
-        filename = 'quotes.%s.html' # tutorial has \ page but it I was getting line cont. error
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
-            self.log('Saved file % s' % filenam) # message to ourself
-
+        self.log('Saved file %s' % filename)
