34e0e50552ca17490bbed0342bb10044ffd07c1a
pes3 <patrick.soulages@gmail.com>
2018-09-05 20:55:34 -0700

    now build a spreadsheet and timestampe it to record what board you got to ride from CI and on what day!

--- BS4/CIshapes.py
+++ BS4/CIshapes.py
@@ -1,4 +1,5 @@
-import bs4, requests, random
+import bs4, requests, random, csv
+from datetime import datetime
 
 def CISurfDesigns(boardesign):
     r = requests.get(boardesign)
@@ -10,3 +11,8 @@ def CISurfDesigns(boardesign):
 shape = CISurfDesigns('http://www.cisurfboards.com/surfboards/')
 
 print('The randomly picked board you get to ride today from CI Surfborads is ' + shape + '!')
+
+# store in an excel file what board you road each day, this will add to the excel file each time it is ran
+with open('index.csv', 'a') as csv_file:
+	writer = csv.writer(csv_file)
+	writer.writerow([shape, datetime.now()])
--- BS4/bs4scrape.py
+++ BS4/bs4scrape.py
@@ -1,5 +1,5 @@
-import bs4
-import requests
+import bs4, requests
+
 r = requests.get('http://www.cisurfboards.com/surfboards/')
 s = bs4.BeautifulSoup(r.text, 'html.parser') # second argument to avoid a warning
 boards = s.select(r'#surfboards > div > ul.grid-details > li:nth-of-type(2) > h2 > a') # had to change to 'nth - of type'
